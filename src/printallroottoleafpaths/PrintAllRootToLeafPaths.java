be41b40bdffe641f4d6435791618dffa60dc3656
Vibhor Vaish <30440477+vaishcr7@users.noreply.github.com>
2018-05-21 16:03:29 +0530

    Update PrintAllRootToLeafPaths.java

--- src/printallroottoleafpaths/PrintAllRootToLeafPaths.java
+++ src/printallroottoleafpaths/PrintAllRootToLeafPaths.java
@@ -23,7 +23,9 @@ public class PrintAllRootToLeafPaths {
       BinaryTreeNode g=new BinaryTreeNode(7);
       c.right=f;
       f.right=g;
-        System.out.println("final answer is : "+paths(a));
+      BinaryTreeNode pol=new BinaryTreeNode(8);
+      g.left=pol;
+      paths(a);
     }
     public static ArrayList<ArrayList<BinaryTreeNode>> paths(BinaryTreeNode root)
     {
@@ -31,44 +33,82 @@ public class PrintAllRootToLeafPaths {
         ArrayList<ArrayList<BinaryTreeNode>> ans=new ArrayList<>();
         ArrayList<ArrayList<BinaryTreeNode>> ans1=new ArrayList<>();
         ArrayList<BinaryTreeNode> temp=new ArrayList<>();
+        
         temp.add(root);
+        
         int t=-1,prevt=0,count=-1;
         while(count==-1 ||  (ans.size()>1 && !ans.get(ans.size()-2) .equals(ans.get(ans.size()-1))))
         {
+            if(ans.size()>1 && !ans.get(ans.size()-2) .equals(ans.get(ans.size()-1)))
+                System.out.println("second condition satisfied");
             BinaryTreeNode parent;
             if(t==-1)
                 parent=root;
             else
             {
+                if(prevt>=ans.size())
+                     break;
                 int len=ans.get(prevt).size();
                 parent=ans.get(prevt).get(len-1);
                 temp=ans.get(prevt);
                 prevt++;
-               }
+            }
+            System.out.print("new base arlist or temp= ");
+            for (int i = 0; i < temp.size(); i++) {
+                System.out.print(" ,"+temp.get(i).data);
+            }
+            System.out.println("");
+            System.out.println("parent is "+parent.data);
             if(parent.left!=null)
             {
-              ArrayList<BinaryTreeNode>p=  temp;
+              ArrayList<BinaryTreeNode>p=new ArrayList<>(temp);
               p.add(parent.left);
               ans.add(p);
+              /*
+               System.out.print("p left=  ");
+            for (int i = 0; i < p.size(); i++) {
+                System.out.print(" ,"+p.get(i).data);
+            }
+                System.out.println("");*/
               t++;
+              System.out.println("t= "+t+" and left node added = "+parent.left.data);
             }
             if(parent.right!=null)
             {
-               ArrayList<BinaryTreeNode>p=  temp;
+               ArrayList<BinaryTreeNode>p=new ArrayList<>(temp);
                p.add(parent.right);
                ans.add(p);
+               /*
+                System.out.print("p right");
+            for (int i = 0; i < p.size(); i++) {
+                System.out.print(" ,"+p.get(i).data);
+            }
+                System.out.println("");
+                */
                t++;
+               System.out.println("t= "+t+" and right node added = "+parent.right.data);
             }
             if(parent.left==null && parent.right==null)
             {
                 correctInd.add(prevt-1);
+                System.out.println("leaf node found");
             }
             count++;
+            System.out.println("count= "+count+" and prevt= "+prevt+" and ans= ");
+            printArr(ans);
         }
-        for (int i = 0; i < correctInd.size(); i++) {
+        for (int i = 0; i < correctInd.size(); i++) 
             ans1.add(ans.get(correctInd.get(i)));
-        }
+        printArr(ans1);
         return ans1;
+    }
+    public static void printArr(ArrayList<ArrayList<BinaryTreeNode>> a)
+    {
+        for(ArrayList<BinaryTreeNode> k:a)
+        { for (int i = 0; i < k.size(); i++)
+        { System.out.print(" ,"+k.get(i).data);   } 
+                System.out.println("");
+        }
     }
 }
 class BinaryTreeNode 
