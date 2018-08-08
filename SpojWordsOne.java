8b4eba72423b40c68519a8af8a6f9d2f79670357
Vibhor Vaish <30440477+vaishcr7@users.noreply.github.com>
2018-08-09 00:36:01 +0530

    Update SpojWordsOne.java

--- SpojWordsOne.java
+++ SpojWordsOne.java
@@ -1,14 +1,150 @@
 package spojwordsone;
 
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Stack;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class SpojWordsOne {
+static class Reader {
 
+        final private int BUFFER_SIZE = 1 << 16;
+        private DataInputStream din;
+        private byte[] buffer;
+        private int bufferPointer, bytesRead;
+
+        public Reader() {
+            din = new DataInputStream(System.in);
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        public Reader(String file_name) throws IOException {
+            din = new DataInputStream(new FileInputStream(file_name));
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        public String readLine() throws IOException {
+            byte[] buf = new byte[64]; // line length
+            int cnt = 0, c;
+            while ((c = read()) != -1) {
+                if (c == '\n') {
+                    break;
+                }
+                buf[cnt++] = (byte) c;
+            }
+            return new String(buf, 0, cnt);
+        }
+
+        public int nextInt() throws IOException {
+            int ret = 0;
+            byte c = read();
+            while (c <= ' ') {
+                c = read();
+            }
+            boolean neg = (c == '-');
+            if (neg) {
+                c = read();
+            }
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+
+            if (neg) {
+                return -ret;
+            }
+            return ret;
+        }
+
+        public long nextLong() throws IOException {
+            long ret = 0;
+            byte c = read();
+            while (c <= ' ') {
+                c = read();
+            }
+            boolean neg = (c == '-');
+            if (neg) {
+                c = read();
+            }
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+            if (neg) {
+                return -ret;
+            }
+            return ret;
+        }
+
+        public double nextDouble() throws IOException {
+            double ret = 0, div = 1;
+            byte c = read();
+            while (c <= ' ') {
+                c = read();
+            }
+            boolean neg = (c == '-');
+            if (neg) {
+                c = read();
+            }
+
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+
+            if (c == '.') {
+                while ((c = read()) >= '0' && c <= '9') {
+                    ret += (c - '0') / (div *= 10);
+                }
+            }
+
+            if (neg) {
+                return -ret;
+            }
+            return ret;
+        }
+
+        private void fillBuffer() throws IOException {
+            bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+            if (bytesRead == -1) {
+                buffer[0] = -1;
+            }
+        }
+
+        private byte read() throws IOException {
+            if (bufferPointer == bytesRead) {
+                fillBuffer();
+            }
+            return buffer[bufferPointer++];
+        }
+
+        public void close() throws IOException {
+            if (din == null) {
+                return;
+            }
+            din.close();
+        }
+    }
     public static void main(String[] args) {
-        
+    try {
+        Reader sc=new Reader();
+        int t=Integer.parseInt(sc.readLine());
+        while(t-->0)
+        {
+            int n=Integer.parseInt(sc.readLine());
+            graph gf=new graph(n);
+            for (int i = 0; i < n; i++) {
+                gf.addVertex(new vertex(sc.readLine()), i);
+            }
+            gf.computeEdges();
+            System.out.println(gf.dfs());
+        }
+    } catch (IOException ex) {
+        System.out.println("exception thrown");
+    }
     }
     
 }
@@ -42,12 +178,12 @@ class graph
    }
    public boolean dfs()
    {
-       if(checkForZeroDegreeVertex())
-           return true;
+       if(!checkForZeroDegreeVertex())
+           return false;
        int verticesVisited=0;
        int stIndex=0;
-       vertex root=null;
-       while(verticesVisited!=numOfVertices && stIndex<=vertexList.size())
+       vertex root;
+       while(verticesVisited!=numOfVertices && stIndex<vertexList.size())
        {
            root=vertexList.get(stIndex++);
            root.visited=true;
@@ -66,7 +202,7 @@ class graph
                }
            }
        }
-       if(stIndex>vertexList.size())
+       if(stIndex>=vertexList.size())
            return false;
       /* stIndex-=1;
        for (int i = 0; i < vertexList.size(); i++) {
@@ -79,16 +215,34 @@ class graph
    {
        for (int i = 0; i < numOfVertices; i++) {
            if(vertexList.get(i).degree==0)
-               return true;
+               return false;// found a vertex with zero degree
        }
-       return false;
+       return true;// if no vertices with zero degree are found
    }
    public void addEdge(vertex a,vertex b)
    {
        adjMat[a.matrixIndex][b.matrixIndex]=1;
        a.degree++;
    }
-    
+   public void computeEdges()
+   {
+       for (int i = 0; i < vertexList.size(); i++) {
+           for (int j = 0; j < vertexList.size(); j++) {
+               if(i!=j && vertexList.get(i).label.charAt(vertexList.get(i).label.length()-1)==vertexList.get(j).label.charAt(0))
+               {
+                   addEdge(vertexList.get(i), vertexList.get(j));
+               }
+           }
+       }
+       System.out.println("adj matrix is ");
+       for (int i = 0; i < vertexList.size(); i++) {
+           for (int j = 0; j < vertexList.size(); j++) {
+               System.out.print(adjMat[i][j]+" , ");
+           }
+           System.out.println("");
+           System.out.println("degree for vertex "+vertexList.get(i).label+" is "+vertexList.get(i).degree);
+       }
+   }
 }
 class vertex
 {
@@ -96,9 +250,9 @@ class vertex
     boolean visited;
     int matrixIndex;
     int degree;
-    public vertex(String label, boolean visited) {
+    public vertex(String label) {
         this.label = label;
-        this.visited = visited;
+        this.visited = false;
         degree=0;
         matrixIndex=-1;
     }
