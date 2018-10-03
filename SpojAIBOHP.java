798c4592348507561f0f7ec64b93b619f28e2dff
Vibhor Vaish <30440477+vaishcr7@users.noreply.github.com>
2018-10-04 00:14:27 +0530

    Update SpojAIBOHP.java

--- SpojAIBOHP.java
+++ SpojAIBOHP.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Spojaibohp {
+public class SpojAIBOHP {
 static class Reader {
 
         final private int BUFFER_SIZE = 1 << 32;
@@ -201,11 +201,10 @@ static class Reader {
         if(i<j)
         {
             System.out.println("not a palindrome");
-        return false;
+            return false;
         }
         return true;
     }
-    
     public static int convToPndmeDownTop(String a,int stLen,int endLen)
     {   
      System.out.println("---------------------FOR STRING  "+a.substring(stLen,endLen)+"   -----------------------");
@@ -214,64 +213,59 @@ static class Reader {
          System.out.println("returning final answer");
          return mp.get(a);
      }*/
+     System.out.println("stlen= "+stLen+" and endLen= "+(endLen-1));
      if(mp.containsKey(a.substring(stLen,endLen)))
      {
          System.out.println("returning stored answer for substring");
          return mp.get(a.substring(stLen,endLen));
      }
-        System.out.println("stlen= "+stLen+" and endLen= "+(endLen-1));
+        System.out.println("before case 1");
      if(checkIfPndme(a.substring(stLen,endLen)))
      {
          System.out.println("case 1");
          mp.put(a,0);
          if(stLen>0 && (endLen<a.length()-1))
             return convToPndmeDownTop(a, stLen-1, endLen+1);
-     }
-     else if(endLen-stLen==0)
+         else
+             return mp.get(a);
+     }    
+        System.out.println("before case 2");
+     if(endLen==stLen)
      {
-         System.out.println("case2a");
+         System.out.println("case2");
          mp.put(""+a.charAt(stLen),0);
+         if(stLen>0 && (endLen<a.length()-1))
+            return convToPndmeDownTop(a, stLen-1, endLen+1);
+         else
+             return mp.get(a);
      }
-     else if((a.charAt(stLen)==a.charAt(endLen)) && endLen-stLen>0)
+        System.out.println("before case 3");
+     if((a.charAt(stLen)==a.charAt(endLen)) && endLen-stLen>0)
      {
-         System.out.println("case 2");
+         System.out.println("case 3");
          if(!(stLen<0 || endLen>a.length()))
             mp.put(a,convToPndmeDownTop(a.substring(stLen-1, endLen+1),stLen-1,endLen+1));  
          else
              return mp.get(a);
      }
-     /*else if((a.charAt(0)==a.charAt(a.length()-1)) && a.length()>2)
-     {
-         //System.out.println("case 2");
-        mp.put(a,convToPndme(a.substring(1, a.length()-1)));
-     }*/
-     else if((a.charAt(stLen)!=a.charAt(endLen)) && endLen-stLen==1)
-     {
-         System.out.println("case 3");
-         mp.put(a,1);
-     }
-     /*
-     else if((a.charAt(0)!=a.charAt(a.length()-1)) && a.length()==2)
-     {
-         //System.out.println("case 3");
-         mp.put(a,1);
-     }*/
-     else
+       System.out.println("before case 4");
+     if((a.charAt(stLen)!=a.charAt(endLen)) && endLen-stLen==1)
      {
          System.out.println("case 4");
+         mp.put(a,1);
+         if(stLen>0 && (endLen<a.length()-1))
+            return convToPndmeDownTop(a, stLen-1, endLen+1);
+         else
+             return mp.get(a);
+     }
+         System.out.println("case 5");
          mp.put(a,(1+
                  (int)Math.min(
                          convToPndmeDownTop(a.substring(stLen,endLen+1),stLen,endLen+1)
                          ,convToPndmeDownTop(a.substring(stLen-1,endLen),stLen-1,endLen)
-                 )));         
-     }
+                 )));       
+         return mp.get(a);
      /*
-     else
-     {
-         //System.out.println("case 4");
-         mp.put(a,(1+(int)Math.min(convToPndme(a.substring(0,a.length()-1)),convToPndme(a.substring(1, a.length()))) ));
-         //mp.put(a,(convToPndme(a.substring(0,a.length()-1))+convToPndme(a.substring(1, a.length()))));
-     }*/
      if(mp.containsKey(a))
      {
          System.out.println("returning value "+mp.get(a)+" for string "+a);
@@ -282,5 +276,6 @@ static class Reader {
         System.out.println("couldn't find the string "+a+" in the map");
          return -1;
      }
+   */
     }
 }
