73d6316da6b837abbb2b0d48c2e30f388326748d
Vibhor Vaish <30440477+vaishcr7@users.noreply.github.com>
2018-11-07 16:06:22 +0530

    Update IBitLongestCommonPrefix.java

--- IBitLongestCommonPrefix.java
+++ IBitLongestCommonPrefix.java
@@ -1,11 +1,6 @@
 package ibitlongestcommonprefix;
 
-import static java.lang.System.out;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Set;
 
 public class IBitLongestCommonPrefix {
 
@@ -13,14 +8,20 @@ public class IBitLongestCommonPrefix {
         ArrayList<String> al=new ArrayList<>();
 //        al.add("shot");
 //        al.add("apple");
-        al.add("orange");
+//        al.add("orange");
 //        al.add("ape");
-        al.add("orangutan");
-        System.out.println(longestCmPfx(al));
+//        al.add("orangutan");
+//        al.add("abcd");
+//        al.add("abde");
+//        al.add("abcf");
+          al.add("ABCD");
+          al.add("ABcd");
+        System.out.println(longestCommonPrefix(al));
     }
-    public static TrieNode root=new TrieNode();
-    public static String longestCmPfx(ArrayList<String> al)
+    public static TrieNode root;
+    public static String longestCommonPrefix(ArrayList<String> al)
     {
+        root=new TrieNode();
         for (int i = 0; i < al.size(); i++) {
             insert(root, al.get(i));
             
@@ -50,7 +51,7 @@ public class IBitLongestCommonPrefix {
 //            System.out.println("");
 //            System.out.println("");
         }
-        System.out.println("kskdcdskhkdshk its children are "+root.numOfChildren);
+//        System.out.println("kskdcdskhkdshk its children are "+root.numOfChildren);
         StringBuilder sba=new StringBuilder();
         int lenOfSmallestString=Integer.MAX_VALUE;
         for (int i = 0; i < al.size(); i++) {
@@ -58,43 +59,53 @@ public class IBitLongestCommonPrefix {
                 lenOfSmallestString=al.get(i).length();
         }
         
-        while(lenOfSmallestString-->0)
+        while(lenOfSmallestString-->0 && root.numOfChildren==1)
         {
-            for (int i = 0; i < 26; i++) {
+            for (int i = 0; i < 123; i++) {
                 if(root.children[i]!=null)
                 {
-                    sba.append((char)97+i);
+//                    sba.append((char)(97+i));
+                    sba.append((char)(i));
                     root=root.children[i];
-                    System.out.println("new sba= "+sba.toString());
+//                    System.out.println("new sba= "+sba.toString());
                     break;
                 }
             }
+//            System.out.println("new root-> "+root.numOfChildren);
         }
-        return sba.toString();
+        if(sba.length()==0)
+            return "";
+        String prefixFromString=al.get(0).substring(0,sba.length());
+        return prefixFromString;
     }
     public static void insert(TrieNode root,String s)
     {
-        System.out.println("insert func called for string "+s);
+//        System.out.println("insert func called for string "+s);
         for (int i = 0; i < s.length(); i++) {
             if(root.isLeaf)
             {
                 root.isLeaf=false;
-                root.children[(int)Character.toLowerCase(s.charAt(i))-97]=new TrieNode();
-                System.out.println("assigning new child to "+Character.toLowerCase(s.charAt(i)));
+//                root.children[(int)Character.toLowerCase(s.charAt(i))-97]=new TrieNode();
+                root.children[(int)s.charAt(i)]=new TrieNode();
+//                System.out.println("assigning new child to "+Character.toLowerCase(s.charAt(i)));
                 root.numOfChildren+=1;
             }
             else
             {
-                if(root.children[(int)Character.toLowerCase(s.charAt(i))-97]==null)
+//                if(root.children[(int)Character.toLowerCase(s.charAt(i))-97]==null)
+                if(root.children[(int)s.charAt(i)]==null)
                 {
-                    System.out.println("adding another path to char "+Character.toLowerCase(s.charAt(i)));
-                    root.children[(int)Character.toLowerCase(s.charAt(i))-97]=new TrieNode();
+//                    System.out.println("adding another path to char "+Character.toLowerCase(s.charAt(i)));
+//                    root.children[(int)Character.toLowerCase(s.charAt(i))-97]=new TrieNode();
+                    root.children[(int)s.charAt(i)]=new TrieNode();
                     root.numOfChildren+=1;
                 }
 //                root=root.children[(int)Character.toLowerCase(s.charAt(i))-97];
             }
-            System.out.println("number of children for "+Character.toLowerCase(s.charAt(i))+" are "+root.numOfChildren);
-            root=root.children[(int)Character.toLowerCase(s.charAt(i))-97];
+//            System.out.println("number of children for "+Character.toLowerCase(s.charAt(i))+" are "+root.numOfChildren);
+//            root=root.children[(int)Character.toLowerCase(s.charAt(i))-97];
+            root=root.children[(int)s.charAt(i)];
+
         }
     }
 }
@@ -102,11 +113,11 @@ class TrieNode
 {
     boolean isLeaf;
     int numOfChildren=0;
-    public TrieNode[] children=new TrieNode[26];
+    public TrieNode[] children=new TrieNode[123];
 
     public TrieNode() {
         isLeaf=true;
-        for (int i = 0; i < 26; i++) {
+        for (int i = 0; i < 123; i++) {
             children[i]=null;
         }
     }
