150d7f699a4cfe7072437124f40a71b893441772
Vibhor Vaish <30440477+vaishcr7@users.noreply.github.com>
2018-12-13 01:13:28 +0530

    Exception in thread "main" java.lang.NullPointerException 	at LRUCache.put(LRUCache.java:19) 	at __Driver__.__helperSelectMethod__(__Driver__.java:16) 	at __Driver__.__helper__(__Driver__.java:38) 	at __Driver__.main(__Driver__.java:83)
    ["LRUCache","put","put","put","put","put","get","put","get","get","put","get","put","put","put","get","put","get","get","get","get","put","put","get","get","get","put","put","get","put","get","put","get","get","get","put","put","put","get","put","get","get","put","put","get","put","put","put","put","get","put","put","get","put","put","get","put","put","put","put","put","get","put","put","get","put","get","get","get","put","get","get","put","put","put","put","get","put","put","put","put","get","get","get","put","put","put","get","put","put","put","get","put","put","put","get","get","get","put","put","put","put","get","put","put","put","put","put","put","put"]
    [[10],[10,13],[3,17],[6,11],[10,5],[9,10],[13],[2,19],[2],[3],[5,25],[8],[9,22],[5,5],[1,30],[11],[9,12],[7],[5],[8],[9],[4,30],[9,3],[9],[10],[10],[6,14],[3,1],[3],[10,11],[8],[2,14],[1],[5],[4],[11,4],[12,24],[5,18],[13],[7,23],[8],[12],[3,27],[2,12],[5],[2,9],[13,4],[8,18],[1,7],[6],[9,29],[8,21],[5],[6,30],[1,12],[10],[4,15],[7,22],[11,26],[8,17],[9,29],[5],[3,4],[11,30],[12],[4,29],[3],[9],[6],[3,4],[1],[10],[3,29],[10,28],[1,20],[11,13],[3],[3,12],[3,8],[10,9],[3,26],[8],[7],[5],[13,17],[2,27],[11,15],[12],[9,19],[2,15],[3,16],[1],[12,17],[9,1],[6,19],[4],[5],[5],[8,1],[11,7],[5,2],[9,28],[1],[2,2],[7,4],[4,22],[7,24],[9,26],[13,28],[11,26]]

--- IBitLRUCache.java
+++ IBitLRUCache.java
@@ -1,60 +1,87 @@
 package ibitlrucache;
 
+import java.util.ArrayDeque;
 import java.util.HashMap;
 
 public class IBitLRUCache {
 
   
     public static void main(String[] args) {
-        Lrucache cache = new Lrucache(2);
-        System.out.println(cache.get(2));   //-1
-        cache.set(2, 6);    //null
-        System.out.println(cache.get(1));   //-1
-        cache.set(1, 5);    //null
-        cache.set(1, 2);    //null
-        System.out.println(cache.get(1));       // 2
-        System.out.println(cache.get(2));       // 6
-    }
+        Lrucache cache = new Lrucache(3);
+        cache.set(1,1);
+        cache.set(2,2);
+        cache.set(3,3);
+        cache.set(4,4);
+        System.out.print(cache.get(4)+"  ,");
+        System.out.print(cache.get(3)+"  ,");
+        System.out.print(cache.get(2)+"  ,");
+        System.out.print(cache.get(1)+"  ,");
+        cache.set(5,5);
+        System.out.print(cache.get(1)+"  ,");
+        System.out.print(cache.get(2)+"  ,");
+        System.out.print(cache.get(3)+"  ,");
+        System.out.print(cache.get(4)+"  ,");
+        System.out.print(cache.get(5)+"  ,");
+//        System.out.println(cache.get(2));   //-1
+//        cache.set(2, 6);    
+//        System.out.println(cache.get(1));   //-1
+//        cache.set(1, 5);    
+//        cache.set(1, 2);    
+//        System.out.println(cache.get(1));   // 2
+//        System.out.println(cache.get(2));   // 6
+    } 
     
 }
 class Lrucache
 {
     int capacity;
     HashMap<Integer,Integer> mp;
-    long t;
-    HashMap<Integer,Long> arval;
-    HashMap<Long,Integer> dept;
+    HashMap<Integer,Integer> c;
+    HashMap<Integer,Integer> d;
+    int t;
     public Lrucache(int capacity) {
         this.capacity = capacity;
         mp=new HashMap<>(capacity);
-        arval=new HashMap<>();
-        dept=new HashMap<>();
+        c=new HashMap<>();
+        d=new HashMap<>();
         t=0;
     }
     public void set(int key,int val)
     {
-        if(mp.size()==capacity)
+//        System.out.println("\n set method called for key: "+key+" and value: "+val+"\n");
+        if(mp.size()==capacity && !mp.containsKey(key))
         {
-            int f=dept.get(t-capacity+1);
-            System.out.print("f= "+f);
-            mp.remove(f);
+              int z=c.get(t-capacity+1);
+//              System.out.println("key to remove: "+z);
+              d.remove(z);
+              c.remove(t-capacity+1);
+              mp.remove(z);
+//              System.out.println("new mp= "+mp);
         }
+        else if(mp.size()==capacity && mp.containsKey(key))
+        {
+                int dz=d.get(key);
+                d.remove(key);
+                c.remove(dz);
+        }
+//        System.out.println("INITIAL mp= "+mp+" and c= "+c+" and d= "+d);
+        t+=1;
         mp.put(key,val);
-        t += 1;
-        arval.put(key,t);
-        dept.put(t, key);
-        dept.remove(t-capacity);
-        System.out.println(" , "+t+" and arval= "+arval+", mp= "+mp+"   ");
+        c.put(t,key);
+        d.put(key, t);
+//        System.out.println("current mp= "+mp+" and c= "+c+" and d= "+d);
     }
     public int get(int key)
     {
+//        System.out.println("\n get method called for key: "+key+"\n");
         if(mp.containsKey(key))
         {
-            t+=1;
-            arval.put(key,t);
-            dept.put(t,key);
-            dept.remove(t-capacity);
-            System.out.print("mp= "+mp+"dept= "+dept+"arval= "+arval+"    ");
+            int dz=d.get(key);
+            d.remove(key);
+            c.remove(dz);
+            t += 1;
+            c.put(t, key);
+            d.put(key, t);
             return mp.get(key);
         }
         return -1;    
