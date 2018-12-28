d23414ba7cb1cb83145f02bbbcc4c672f8958543
Vibhor Vaish <30440477+vaishcr7@users.noreply.github.com>
2018-12-28 16:23:47 +0530

    AC in first try!!! Thats like mothafuckin magic .
    shortest unique prefix Trie question

--- IBitShrUqPfx.java
+++ IBitShrUqPfx.java
@@ -6,10 +6,10 @@ public class IBitShrUqPfx {
 
     public static void main(String[] args) {
         ArrayList<String> m=new ArrayList<>();
-        m.add("zebra");
-        m.add("dogg");
-        m.add("dove");
-        m.add("dack");
+        m.add("guwnn");
+        m.add("q");
+        m.add("nz");
+//        m.add("");
         System.out.println(sqp(m));
     }
     public static ArrayList<String> sqp(ArrayList<String> al)
@@ -22,51 +22,87 @@ public class IBitShrUqPfx {
         Trie p=t;
         for (String string : al) {
             t=p;
-//            System.out.println("string = "+string);
+            System.out.println("string = "+string);
             for (int i = 0; i < string.length(); i++) {
-                int n=((int)string.charAt(i))-97;
+                int n=((int)string.charAt(i))-97; 
                 if(t.edges[n]==null)
                 {
-                    Trie y=new Trie();
-                    t.edges[n]=y;
-//                    System.out.println("not visited yet letter= "+string.charAt(i));
+                    System.out.println("EARLIER was being shared "+t.sharedBy+" words= "+string.charAt(i)+" for string "+string);
+                    t.sharedBy+=1;
+                    t.edges[n]=new Trie();
                     if(i!=string.length()-1)
                     {
-                        y.endOfWord=false;
+                        t.edges[n].endOfWord=false;
                     }
-//                    else
-//                        System.out.print("  end of word reached \n");
+                    else
+                        System.out.print("  end of word reached \n");
                 }
                 else
                 {
+                    t.edges[n].sharedBy+=1;
                     t.sharedBy+=1;
-//                    System.out.println("shared by other words= "+string.charAt(i));
+                    System.out.println("shared already by "+t.edges[n].sharedBy+" words= "+string.charAt(i)+" for string "+string);
                 }
                 t=t.edges[n];
             }
-        }
-        
+        }        
+        System.out.println("\n\nmaking unique prefixes \n");
         for (String string : al) {
             t=p;
+//            Trie prev=null;
             System.out.println("\nstring = "+string);
             for (int i = 0; i < string.length(); i++) {
-                if(t.sharedBy>0)
+                System.out.println(t.sharedBy+" for char "+string.charAt(i));
+                if(t.sharedBy>1 && i!=string.length()-1)
                 {
-                    System.out.print(t.edges[((int)string.charAt(i))-97].sharedBy);
+//                    System.out.println("filled by");
+//                    for (int j = 0; j < 26; j++) {
+//                        if(t.edges[j]!=null)
+//                            System.out.print(" , "+t.sharedBy);
+//                        else
+//                            System.out.print(" , "+((char)(97+j)));
+//                    }
+//                    System.out.println("");
+                    System.out.print(t.sharedBy);
+//                    prev=t;
                     t=t.edges[((int)string.charAt(i))-97];
                     System.out.println(" and used by other words= "+string.charAt(i));
                 }
-                else if(i==string.length()-1)
+                else if(i==string.length()-1 && t.sharedBy>1)
                 {
                     ans.add(string);
                     System.out.print("  end of word reached \n");
                     break;
                 }
+                else if(i==string.length()-1 && t.sharedBy<=1)
+                {
+                    ans.add(string.substring(0, i));
+                    System.out.print("  end of word reached but a shorter prefix possible \n");
+                    break;
+                }
                 else
                 {
-                    String k=string.substring(0,i+1);
+                    String k;
+//                    System.out.println("prev is ");
+//                    for (int j = 0; j < 26; j++) {
+//                        if(prev.edges[j]!=null)
+//                            System.out.print(" , "+prev.sharedBy);
+//                        else
+//                            System.out.print(" , "+((char)(97+j)));
+//                    }
+//                    System.out.println("");
+//                    System.out.println("prev char= "+(string.charAt(i-1))+"  shared by = "+prev.edges[((int)string.charAt(i-1))-97].sharedBy);
+                    if(t.sharedBy>1)
+                    {
+                        k=string.substring(0,i+1);
+                    }
+                    else
+                    {
+                        k=string.substring(0,i);
+                    }
                     ans.add(k);
-                    System.out.println("sole component= "+string.charAt(i));
+                    System.out.print(t.sharedBy);
+                    System.out.println("  sole component= "+string.charAt(i));
                     break;
                 }
             }
